49a97cfea0fafecafd5981705c990d7d80516bd6
THITAI <evansnt9619@gmail.com>
2024-03-12 08:49:34 +0300

    no postgresql

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ from auth import Auth
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['JWT_SECRET_KEY']= os.environ.get('JWT_SECRET_KEY')
 
 
